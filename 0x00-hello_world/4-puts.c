38cf3afe1e985fd0bd7624b7dd9eb44d2842fdf7
Tassuo7 <abdelhamid.jack@gmail.com>
2023-02-16 18:27:09 -0800

    bisous

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/*
+/**
  * main - A program that prints a line with puts function
+ * Description: A C program that prints with put function
  * Return: 0 (success)
  */
 int main(void)
