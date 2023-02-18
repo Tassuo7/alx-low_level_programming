0ffede7c35ede658e169eca794bf0bec0f373a32
Tassuo7 <abdelhamid.jack@gmail.com>
2023-02-17 18:07:24 -0800

    ghlet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,9 @@
 
 int main(void)
 {
-	int d;
 	char low;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
+	for (low = 'z'; low >= 'a'; low--)
 		putchar(low);
 	putchar('\n');
 
