f96553716a192fe0262bb64e36bad05e7d179da1
Tassuo7 <abdelhamid.jack@gmail.com>
2023-02-21 17:03:32 -0800

    go

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - start
+ *
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int print[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
+
+	for (i = 0; i < 8; i++)
+		{
+		_putchar(print[i]);
+		}
+	_putchar('\n');
+	return (0);
+}
