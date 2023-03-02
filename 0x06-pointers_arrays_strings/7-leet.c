0788c4fa575b0a85a7c09d44387e9489a61e51a3
Tassuo7 <abdelhamid.jack@gmail.com>
2023-03-01 20:03:10 -0800

    hal3aar

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,15 +9,15 @@ char *leet(char *s)
 {
 	int i = 0, j;
 	char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
-    	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	     str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
 	while (s[i])
-		{
+	{
 		for (j = 0; j < 10; j++)
 			if (s[i] == str[j])
 				s[i] = subs[j];
 			i++;
-		}
+	}
 
 	return (s);
 }
