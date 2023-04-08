d21a7c5b7efaf2995a8463d3fdbc2de135a7cdab
Tassuo7 <abdelhamid.jack@gmail.com>
2023-04-08 11:37:29 -0700

    applying the ressources

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,13 +10,10 @@ void print_binary(unsigned long int n)
 {
 	if (n)
 		_putchar('0');
-	else if (n == 1)
-		_putchar('1');
 	else
 	{
-		if (n >> 1)
+		if (!(n >> 1) && ((n >> 1) != '1'))
 			print_binary(n >> 1);
 		_putchar(n & 1);
-		_putchar('0');
 	}
 }
