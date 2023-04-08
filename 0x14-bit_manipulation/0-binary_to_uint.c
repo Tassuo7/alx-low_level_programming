56cae8e23a0974cf153d393fa75a1f4780f8872d
Tassuo7 <abdelhamid.jack@gmail.com>
2023-04-08 09:21:18 -0700

    convertion

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary
+ *
+ * Return: the converted number, or 0
+ */
+unsigned int binary_to_uint(const char *b);
+{
+	unsigned int unit = 0;
+	int prcr, pwr;
+
+	if (!b)
+		return (0);
+
+	for (prcr = 0 ; b[prcr] != '/0' ; prcr++)
+	{
+		if (b[prcr] != '0' && b[prcr] != '1')
+		return (0);
+	}
+
+	for (pwr = 0 ; prcr >= 0 ; prcr--)
+		unit += b[prcr] * pow(2, pwr);
+
+	return (unit);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include<math.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
