727b817e2350aea37bcbf53e8014029483d5b69c
Tassuo7 <abdelhamid.jack@gmail.com>
2023-04-09 10:06:37 -0700

    traitement erreur

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,16 +2,16 @@
 
 /**
  * get_bit - returns the value of a bit at a given index.
- * @n: unsigned long int.
- * @index: is the index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
  *
- * Return: value of the bit at index or -1.
+ * Return: value of the bit.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	if (n == 0 && index =< 63)
+	if (n == 0 && index <= 63)
 		return (0);
 
 	for (i = 0; i <= 63; n >>= 1, i++)
