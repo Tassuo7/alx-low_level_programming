ad7a65e501a8a8439e04837b17807757632d0d7a
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-22 17:29:56 -0700

    iwanna win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Tassuo7/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrand.so
-export LD_PRELOAD=/tmp/nrand.so
+wget -P /tmp https://github.com/Tassuo7/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
