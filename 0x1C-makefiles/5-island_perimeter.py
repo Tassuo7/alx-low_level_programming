5795568140c880633ecd4b35eda9ab3ae3e66ecd
Tassuo7 <abdelhamid.jack@gmail.com>
2023-07-26 12:54:08 -0700

    mandatory

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island
+    """
+    isperim = 0
+    verz = len(grid[0])
+    horz = len(grid)
+    for h in range(horz):
+        for v in range(verz):
+            if grid[h][v] == 1:
+                isperim += 4
+                if h > 0 and grid[h - 1][v] == 1:
+                    isperim -= 2
+                if h > 0 and grid[h][v - 1] == 1:
+                    isperim -= 2
+    return isperim
