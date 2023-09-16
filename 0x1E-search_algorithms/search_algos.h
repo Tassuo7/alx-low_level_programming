1950fc56d234833e49ac5c5f0031a62ebf81f564
Tassuo7 <abdelhamid.jack@gmail.com>
2023-09-16 03:46:49 -0700

    search algo

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
+
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+#endif
