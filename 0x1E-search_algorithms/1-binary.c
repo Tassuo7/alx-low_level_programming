3a1bb9f2f91ab4de60b4c08b856124b95590540b
Tassuo7 <abdelhamid.jack@gmail.com>
2023-09-16 06:25:31 -0700

    search algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,53 @@
 #include "search_algos.h"
 
 /**
+ * print_array - Prints the array
+ * @array: The array to be printed
+ * @size: size of the array
+ */
+
+void print_array(int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
+
+/**
+ * re_search - search the value
+ * @array: The array to search in
+ * @size: size of the array
+ * @value: the value to be searched
+ * Return: the index
+ */
+
+int re_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+
+	if (!array || size == 0)
+		return (-1);
+	if (size % 2 == 0)
+		half--;
+	print_array(array, size);
+
+	if (array[half] == value)
+		return (half);
+	if (array[half] > value)
+		return (re_search(array, half, value));
+	half++;
+	return (re_search(array + half, size - half, value) + half);
+}
+
+/**
  * binary_search - searches for a value in a sorted array
  * using the Binary search algorithm
  * @array: the first element of the array to search in
@@ -11,32 +58,10 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t first = 0, last = size - 1, half, i;
+	size_t idx;
 
-	if (!array || size == 0)
+	idx = re_search(array, size, value);
+	if (array[idx] != value)
 		return (-1);
-
-	while (first <= last)
-	{
-		printf("Searching in array: ");
-		for (i = 0; i < size; i++)
-		{
-			printf("%d", array[i]);
-			if (i < size - 1)
-				printf(", ");
-			else
-				printf("\n");
-		}
-		half = first + (last - first) / 2;
-		if (array[half] == value)
-			return (half);
-		if (array[half] < value)
-			first = half + 1;
-		else
-			last = half - 1;
-		aray = array + first;
-		size = last - first + 1;
-	}
-
-	return (-1);
+	return (idx);
 }
